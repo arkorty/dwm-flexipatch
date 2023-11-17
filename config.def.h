da5d669f1c72bdd463d619ba2dff79b95064dd05
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-11-18 01:35:18 +0530

    Update tags

--- config.def.h
+++ config.def.h
@@ -451,7 +451,7 @@ static char *tagicons[][NUMTAGS] =
 #endif // NAMETAG_PATCH
 {
 	//[DEFAULT_TAGS]      = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
-  [DEFAULT_TAGS]        = { "term", "edit", "www", "file", "comm", "media", "proc", "misc", "misc" },
+	[DEFAULT_TAGS]        = { "term", "edit", "www", "util", "media", "misc", "misc", "bg", "bg" },
 	[ALTERNATIVE_TAGS]    = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
 	[ALT_TAGS_DECORATION] = { "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>", "<8>", "<9>" },
 };
@@ -502,15 +502,15 @@ static const Rule rules[] = {
   RULE(.class   = "Galculator",       .tags = 0,       .isfloating = 1 )
   RULE(.class   = "Lxappearance",     .tags = 0,       .isfloating = 1 )
   RULE(.class   = "Localsend",        .tags = 0,       .isfloating = 1 )
+  RULE(.class   = "Code",             .tags = 1 << 1,  .isfloating = 0 )
   RULE(.class   = "Gimp",             .tags = 1 << 1,  .isfloating = 1 )
   RULE(.class   = "firefox",          .tags = 1 << 2,  .isfloating = 0 )
-  RULE(.class   = "mpv",              .tags = 1 << 5,  .isfloating = 0 )
   RULE(.class   = "Nemo",             .tags = 1 << 3,  .isfloating = 0 )
-  RULE(.class   = "Code",             .tags = 1 << 1,  .isfloating = 0 )
-  RULE(.class   = "Spotify",          .tags = 1 << 5,  .isfloating = 0 )
-  RULE(.class   = "discord",          .tags = 1 << 4,  .isfloating = 0 )
-  RULE(.class   = "TelegramDesktop",  .tags = 1 << 4,  .isfloating = 0 )
-  RULE(.class   = "qBittorrent",      .tags = 1 << 6,  .isfloating = 0 )
+  RULE(.class   = "discord",          .tags = 1 << 5,  .isfloating = 0 )
+  RULE(.class   = "TelegramDesktop",  .tags = 1 << 5,  .isfloating = 0 )
+  RULE(.class   = "Spotify",          .tags = 1 << 4,  .isfloating = 0 )
+  RULE(.class   = "qBittorrent",      .tags = 1 << 8,  .isfloating = 0 )
+  RULE(.class   = "obs",              .tags = 1 << 7,  .isfloating = 0 )
 	#if RENAMED_SCRATCHPADS_PATCH
 	RULE(.instance = "spterm", .scratchkey = 's', .isfloating = 1)
 	#elif SCRATCHPADS_PATCH
